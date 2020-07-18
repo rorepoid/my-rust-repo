8b3ffd92124fefddb610399231bca12363132a43
Roberto Orellana <rorepoid@gmail.com>
2020-07-18 02:12:53 -0500

    feat: Use username and name from input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-fn main() {
-    let mut name = "rorepoid";
-    println!("{} says Hello World", name);
+use std::io;
 
-    name = "Roberto Orellana";
-    println!("But the real name of is: {}", name);
+fn main() {
+    println!("Enter your username:");
+    let  mut username = String::new();
+    io::stdin().read_line(&mut username).expect("Couldn't read username");
+    let username = username.trim();
+
+    println!("Enter your name");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Couldn't read name");
+    let name = name.trim();
+
+    println!("{} says Hello World", username);
+    println!("But the real name of {} is: {}", username, name);
     
 }
