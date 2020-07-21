d57981efb6403e505d44974d560884f4edcd8ecd
Roberto Orellana <rorepoid@gmail.com>
2020-07-20 23:59:54 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     io::stdin().read_line(&mut username).expect("Couldn't read username");
     let username = username.trim();
 
-    println!("Enter your name");
+    println!("Enter your name:");
     let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("Couldn't read name");
+    let variable: String = io::stdin().read_line(&mut name).expect("Couldn't read name").to_string();
+    println!("variable content: {}", variable);
     let name = name.trim();
 
-    println!("{} says Hello World", username);
+    println!("{} says El Psy Kongroo", username);
     println!("But the real name of {} is: {}", username, name);
-    
 }
