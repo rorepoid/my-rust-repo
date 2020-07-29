f3dbbec2b8f1e6e074c6fdb3d441f0586b0f99f1
Roberto Orellana <rorepoid@gmail.com>
2020-07-29 00:22:54 -0500

    style: Change position order to constants and add comments to requests

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::io;
 
 const MESSAGE: &str = "El Psy Kongroo";
-const USERNAME_REQUEST: &str = "Enter your username";
 const NAME_REQUEST: &str = "Enter your name";
+const USERNAME_REQUEST: &str = "Enter your username";
 
 fn main() {
+    // ask for username
     println!("{}", USERNAME_REQUEST);
-    let  mut username = String::new();
+    let mut username = String::new();
+
     io::stdin().read_line(&mut username).expect("Couldn't read username");
     let username = username.trim();
 
+    // ask for name
     println!("{}", NAME_REQUEST);
     let mut name = String::new();
+
     io::stdin().read_line(&mut name).expect("Couldn't read name").to_string();
     let name = name.trim();
 
