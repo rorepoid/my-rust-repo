2de7e3afee1704b290dc68d0cea65d2f5d39e1d5
Roberto <rorepoid@gmail.com>
2021-05-16 13:11:20 -0500

    Run minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-mod user;
+mod minigrep;
 
 fn main() {
-    let username: String = "".to_string();
-    let name: String = "".to_string();
-
-    let mut user: user::User = user::User::new(username, name);
-    user.ask_username();
-    user.ask_name();
-    user.say_username();
-    user.say_name();
+    minigrep::start();
 }
