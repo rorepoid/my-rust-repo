4ed27085b2ee80accce30488d1ab403cb779f790
Roberto <rorepoid@gmail.com>
2021-05-16 13:09:44 -0500

    Fix typo

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -34,7 +34,7 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            return Err("Not enought arguments");
+            return Err("Not enough arguments");
         }
         let query: String = args[1].clone();
         let filename: String = args[2].clone();
