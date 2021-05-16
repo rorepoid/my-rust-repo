1a50ffb4d00b767d937481480d1a6ea66d5c03ed
Roberto <rorepoid@gmail.com>
2021-05-16 13:00:14 -0500

    Organize config arguments in a struct

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -3,20 +3,25 @@ use std::fs;
 
 pub fn start() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename): (&str, &str) = parse_config(&args);
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents: String = fs::read_to_string(filename)
+    let contents: String = fs::read_to_string(config.filename)
         .expect("Could't find file");
 
     println!("With text: \n{}", contents);
 }
 
-pub fn parse_config(args: &[String]) -> (&str, &str) {
-    let query: &str = &args[1];
-    let filename: &str = &args[2];
+pub struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+pub fn parse_config(args: &[String]) -> Config {
+    let query: String = args[1].clone();
+    let filename: String = args[2].clone();
+
+    Config { query, filename }
 }
