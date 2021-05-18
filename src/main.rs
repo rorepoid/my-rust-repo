16f67e96231448ce906860f6e0fd697a7d91e7d6
Roberto <rorepoid@gmail.com>
2021-05-17 23:49:44 -0500

    refactor: Separate minigrep in lib file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-mod minigrep;
+use std::env;
+use std::process;
+
+use my_rust_repo::Config;
 
 fn main() {
-    minigrep::start();
+    let args: Vec<String> = env::args().collect();
+    let config: Config = Config::new(&args).unwrap_or_else(|err: &str| {
+        println!("Problem passing arguments, {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = my_rust_repo::run(config) {
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
 }
