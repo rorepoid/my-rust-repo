e924c14a4c9a1d4f94b244cd97a928da95d304a9
Roberto <rorepoid@gmail.com>
2021-05-18 01:51:05 -0500

    fix: Use eprintln!() macro on error

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use my_rust_repo::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::new(&args).unwrap_or_else(|err: &str| {
-        println!("Problem passing arguments, {}", err);
+        eprintln!("Problem passing arguments, {}", err);
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = my_rust_repo::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1);
     }
 }
